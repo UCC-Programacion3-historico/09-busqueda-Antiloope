2ee39d325c36d79565c7509225f2504ab9eb945b
Antiloope <rodrigopizarro1234@gmail.com>
2017-11-22 11:09:59 -0300

    Listo el ejercicio 1

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -2,11 +2,17 @@
 
 void fn(int x, int v[], int ini, int fin, int pos, bool retorno);
 
+void fn(int v[],int pos,int fin,int x);
+
 int main() {
     std::cout << "Ejercicio 09/01\n" << std::endl;
     return 0;
 }
 
+void fn(int v[],int pos,int fin,int x){
+    if(pos >= fin || v[pos] == x) return;
+    fn(v,pos+1,fin,x);
+}
 
 void fn(int x, int v[], int ini, int fin, int pos, bool retorno) {
     pos = ini;
